64a7bdaca4a2dd7ff3ec84de079cc29c56584a96
GITA <jgitanjali592@gmail.com>
2024-03-24 19:08:33 +0530

    this code for factorial

---
+++ Factorial.js
@@ -0,0 +1,17 @@
+//  Q-give integer 'n' find the factorial of the integer
+
+ function factorial(n){
+
+    let result = 1
+
+    for(let i=1; i<=n; i++){
+        result = result * i
+   
+ }
+
+   return result
+ }
+
+ console.log(factorial(0));
+ console.log(factorial(1));
+ console.log(factorial(5));
