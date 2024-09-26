51211e526370f6723990e3048fded8df0b8347e1
GITA <jgitanjali592@gmail.com>
2024-09-26 07:49:14 +0530

    this code foriterative power

---
+++ iterative.js
@@ -0,0 +1,12 @@
+function mypower(x,n){
+  let res =1;
+  while(n>0){
+    if(n %2!==0)
+      res = res *x;
+    x= x*x;
+    n= Math.floor(n/2);
+  }
+  return res;
+}
+
+console.log(mypower(4,5)); // output: 8
